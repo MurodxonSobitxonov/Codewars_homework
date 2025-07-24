430c42e21b4997f9a8c838ba4a65c82c5853e09c
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-07-24 21:15:59 +0500

    changet

---
+++ index.js
@@ -0,0 +1,17 @@
+// function stringToArray(string){
+//   return string.split(' ');
+// }
+
+// function invert(array) {
+//   return array.map( x => x === 0 ? x : -x);;
+// }
+
+// function abbrevName(name){
+//   return name.split(' ').map(i => i[0].toUpperCase()).join('.')
+// }
+ 
+// const makeUpperCase = str => str.toUpperCase();
+
+// function greet(name){
+//   return `Hello, ${name} how are you doing today?`;
+// }
