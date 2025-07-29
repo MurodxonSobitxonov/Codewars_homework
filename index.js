7783c1c56b7936ec7c960d099dc8abb66979aa8f
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-07-29 13:41:19 +0630

    changet

--- index.js
+++ index.js
@@ -15,3 +15,34 @@
 // function greet(name){
 //   return `Hello, ${name} how are you doing today?`;
 // }
+
+// 20 - dars_Homework
+
+// function removeChar(str){
+//   return str.slice(1, -1);
+// };
+
+//------------------------------------------------
+
+// function descendingOrder(n){
+//   return parseInt(String(n).split('').sort().reverse().join(''))
+// }
+
+//-------------------------------------------------
+
+// function sumTwoSmallestNumbers(numbers) {
+//   numbers = numbers.sort(function(a, b){return a - b; });
+//   return numbers[0] + numbers[1];
+// }
+
+//--------------------------------------------------
+
+// const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+
+//--------------------------------------------------
+
+// function getMiddle(s)
+// {
+//   return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+// }
+
