50990b5ceae910f5594d932812a45c7c8c18833e
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-09-22 23:14:42 +0500

    chatget

--- index.js
+++ index.js
@@ -204,3 +204,22 @@
 // function check(a, x) {
 //   return a.includes(x)
 //   }
+
+// 10.
+
+// function howMuchILoveYou(nbPetals) {
+//   switch ((nbPetals - 1) % 6) {
+//       case 1:
+//           return "a little";
+//       case 2:
+//           return "a lot";
+//       case 3:
+//           return "passionately";
+//       case 4:
+//           return "madly";
+//       case 5:
+//           return "not at all";
+//       default:
+//           return "I love you";
+//   }
+// }
