6adce92a35c338b9ab03cef7311fcff4f5e2f0f8
MurodxonSobitxonov <kosonsoy5256@gmail.com>
2025-09-27 00:40:08 +0500

    changet

--- index.js
+++ index.js
@@ -223,3 +223,139 @@
 //           return "I love you";
 //   }
 // }
+
+
+
+//=========================================================================
+//1
+// function makeNegative(num) {
+//   return num > 0 ? -num : num;
+// }
+
+//=========================================================================
+//2
+// function boolToWord(bool) {
+//   return bool ? "Yes" : "No";
+// }
+
+//=========================================================================
+//3
+// function getAge(inputString) {
+//   return parseInt(inputString[0]);
+// }
+
+//=========================================================================
+//4
+// // function sumArray(array) {
+//   if (!array || array.length <= 2) return 0;
+
+//   const max = Math.max(...array);
+//   const min = Math.min(...array);
+
+//   let sum = array.reduce((acc, val) => acc + val, 0);
+
+//   return sum - max - min;
+// }
+
+//=========================================================================
+//5
+// function greet(name, owner) {
+//   return name === owner ? 'Hello boss' : 'Hello guest';
+// }
+
+//=========================================================================
+
+//6
+
+// function enough(cap, on, wait) {
+//   let spaceLeft = cap - on;
+//   return spaceLeft >= wait ? 0 : wait - spaceLeft;
+// }
+
+//=========================================================================
+//7
+// function XO(str) {
+//   str = str.toLowerCase();
+
+//   let xCount = 0;
+//   let oCount = 0;
+
+//   for (let char of str) {
+//     if (char === 'x') xCount++;
+//     else if (char === 'o') oCount++;
+//   }
+
+//   return xCount === oCount;
+// }
+
+//=========================================================================
+//8
+
+// function getGrade(s1, s2, s3) {
+//   const average = (s1 + s2 + s3) / 3;
+
+//   if (average >= 90) return 'A';
+//   else if (average >= 80) return 'B';
+//   else if (average >= 70) return 'C';
+//   else if (average >= 60) return 'D';
+//   else return 'F';
+// }
+
+//=========================================================================
+//9
+// function between(a, b) {
+//   let result = [];
+//   for (let i = a; i <= b; i++) {
+//     result.push(i);
+//   }
+//   return result;
+// }
+
+//=========================================================================
+//10
+// function isPalindrome(x) {
+
+//   const lower = x.toLowerCase();
+
+//   const reversed = lower.split('').reverse().join('');
+
+//   return lower === reversed;
+// }
+
+//=========================================================================
+//11
+// function betterThanAverage(classPoints, yourPoints) {
+//   let sum = 0;
+
+//   for (let i = 0; i < classPoints.length; i++) {
+//     sum += classPoints[i];
+//   }
+
+//   let average = sum / classPoints.length;
+//   return yourPoints > average;
+// }
+
+//=========================================================================
+//12
+// const binaryArrayToNumber = arr => {
+//   return parseInt(arr.join(''), 2);
+// };
+
+//=========================================================================
+//13
+// function century(year) {
+//   return Math.ceil(year / 100);
+// }
+//=========================================================================
+//14
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//   let diff = dadYearsOld - 2 * sonYearsOld;
+//   if (diff < 0) {
+//     return -diff;
+//   } else {
+//     return diff;
+//   }
+// }
+//=========================================================================
+//15
+
